3a00bf25ada09c5724e96022f816ca43f0764421
aleksej <monsarda@yandex.ru>
2019-05-28 09:33:18 +0300

    update

--- app/src/main/java/ru/intera/smscollector/MainActivity.java
+++ app/src/main/java/ru/intera/smscollector/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +14,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<String> sms;
     public FloatingActionButton fab;
     public AlertDialog.Builder ad;
-    public EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,33 +51,36 @@ public class MainActivity extends AppCompatActivity {
         sms = new ArrayList<>();
         fab = findViewById(R.id.fab);
         recyclerView = findViewById(R.id.sms_list);
-        message = findViewById(R.id.message_text);
-        Log.d("TAG", "initGui: " + message);
     }
 
     private void initEvents() {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog();
+                showDialog(v);
             }
         });
 
     }
 
-    private void showDialog() {
+    private void showDialog(View v) {
 
-        ad.setView(R.layout.prompt);
-
+        final EditText input = new EditText(this);
+        ad.setView(input);
+        ad.setTitle("Ваше сообщение");
 
         ad.setPositiveButton("Отправить",  new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                Log.d("TAG", "onClick: " + message);
-//                sendMessage(message.getText().toString());
+                String msg = input.getText().toString();
+                try {
+                    sendMessage(msg);
+                }catch (Exception e) {
+                    Log.e("Exception", "Field is not empty", e);
+                }
+
             }
         });
         ad.show();
-
     }
 
     private void sendMessage(String message) {
